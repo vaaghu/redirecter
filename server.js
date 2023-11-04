ec27768f0fd4a6dc14d7b36c0e281dc245323769
vaaghu <vignesh021102@gmail.com>
2023-11-04 14:43:21 +0530

    PORT -> 443

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const app = express()
-const PORT = 2999
+const PORT = 443
 
 const {youtube,linkedin} = require("./controllers/main.js")
 const {checkAndroid, page} = require("./controllers/others.js")
